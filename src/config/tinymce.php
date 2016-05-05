009ae17a7e603615ee2c59db3e65323a64fba92e
Pintoko Daruadi <pintoko.daruadi@kaskusnetworks.com>
2016-05-06 01:48:09 +0700

    change default height dialog box codesample

--- src/config/tinymce.php
+++ src/config/tinymce.php
@@ -12,9 +12,9 @@ return [
 		"plugins" => [
 	         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
 	         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-	         "save table contextmenu directionality emoticons template paste textcolor"
+	         "save table contextmenu directionality emoticons template paste textcolor codesample"
 		],
-		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+		"toolbar" => "insertfile codesample | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
 	],
 
 	// Custom
@@ -29,7 +29,7 @@ return [
 	         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
 	         "save table contextmenu directionality emoticons template paste textcolor"
 		],
-		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
 	],*/
 
 ];
